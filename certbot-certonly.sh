d76cd1ff746001e4534e486c227d4b8877b00534
Jacob Parry <jacob@jacobparry.ca>
2020-02-07 17:57:47 -0500

    Always use config in helper scripts, refresh after cert issuance and renewal

--- certbot-certonly.sh
+++ certbot-certonly.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/local/bin/certbot-auto certonly -c /usr/local/etc/letsencrypt/cli.ini "$@"
+haproxy-refresh
--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/certbot-auto renew "$@"
+/usr/local/bin/certbot-auto -c /usr/local/etc/letsencrypt/cli.ini renew "$@"
+haproxy-refresh
